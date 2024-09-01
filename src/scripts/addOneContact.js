c7035703fb19537694f1dff2cba905498ae23233
cel3ntano <adriano2024celentano@gmail.com>
2024-09-01 19:25:29 +0200

    addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,13 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
+const addOneContact = async () => {
+  const buffer = await readContacts();
+  const prevContacts = buffer ? JSON.parse(buffer) : [];
+  const updatedContacts = [...prevContacts, createFakeContact()];
+  await writeContacts(JSON.stringify(updatedContacts, null, 2));
+};
 addOneContact();
+
+
