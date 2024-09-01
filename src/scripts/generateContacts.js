3580a1c501baae74f1f9013ef3ad5c71451b6092
cel3ntano <adriano2024celentano@gmail.com>
2024-09-01 16:59:34 +0200

    generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const buffer = await readContacts();
+  const prevContacts = buffer ? JSON.parse(buffer) : [];
+  const generatedContacts = [];
+  for (let i = 0; i < number; i++) {
+    generatedContacts.push(createFakeContact());
+  }
+
+  const updatedContacts = [...prevContacts, ...generatedContacts];
+  await writeContacts(JSON.stringify(updatedContacts, null, 2));
+};
 
 generateContacts(5);
