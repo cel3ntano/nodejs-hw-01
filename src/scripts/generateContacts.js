5aeed493ee886e73fe57919da67ddd67544f2931
cel3ntano <adriano2024celentano@gmail.com>
2024-09-01 19:30:41 +0200

    generateContacts add try...catch statement

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -3,15 +3,19 @@ import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 const generateContacts = async (number) => {
-  const buffer = await readContacts();
-  const prevContacts = buffer ? JSON.parse(buffer) : [];
-  const generatedContacts = [];
-  for (let i = 0; i < number; i++) {
-    generatedContacts.push(createFakeContact());
-  }
+  try {
+    const buffer = await readContacts();
+    const prevContacts = buffer ? JSON.parse(buffer) : [];
+    const generatedContacts = [];
+    for (let i = 0; i < number; i++) {
+      generatedContacts.push(createFakeContact());
+    }
 
-  const updatedContacts = [...prevContacts, ...generatedContacts];
-  await writeContacts(JSON.stringify(updatedContacts, null, 2));
+    const updatedContacts = [...prevContacts, ...generatedContacts];
+    await writeContacts(JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 generateContacts(5);
