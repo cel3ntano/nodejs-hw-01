4ffde59011c3fc1b70c933359aee63e9a7b70381
cel3ntano <adriano2024celentano@gmail.com>
2024-09-01 20:51:53 +0200

    removeAllContacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,12 @@
-export const removeAllContacts = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    const emptyContactsArray = [];
+    await writeContacts(JSON.stringify(emptyContactsArray, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeAllContacts();
