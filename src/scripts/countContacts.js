68c295ceffeaac8329e2dba4feaaf06ec2158688
cel3ntano <adriano2024celentano@gmail.com>
2024-09-01 20:46:17 +0200

    countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,15 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const buffer = await readContacts();
+    const contacts = buffer ? JSON.parse(buffer) : [];
+    return contacts.reduce((acc) => {
+      return acc + 1;
+    }, 0);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 console.log(await countContacts());
