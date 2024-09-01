519b946fb3c5b0168a819f2e6b06dd4acad6b4e4
cel3ntano <adriano2024celentano@gmail.com>
2024-09-01 19:29:49 +0200

    addOneContact add try...catch statement

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -3,11 +3,13 @@ import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 const addOneContact = async () => {
-  const buffer = await readContacts();
-  const prevContacts = buffer ? JSON.parse(buffer) : [];
-  const updatedContacts = [...prevContacts, createFakeContact()];
-  await writeContacts(JSON.stringify(updatedContacts, null, 2));
+  try {
+    const buffer = await readContacts();
+    const prevContacts = buffer ? JSON.parse(buffer) : [];
+    const updatedContacts = [...prevContacts, createFakeContact()];
+    await writeContacts(JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
 };
 addOneContact();
-
-
